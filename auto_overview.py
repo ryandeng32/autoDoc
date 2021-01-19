ffcfe3d58beb87b48de5e19d9cb61fb3572f16d9
Ryan Deng <ryan.deng32@gmail.com>
2021-01-19 13:42:06 -0500

    feat: Add fix to D412 that also converts these docstrings to follow the Google format

--- auto_overview.py
+++ auto_overview.py
@@ -9,17 +9,8 @@ from collections import defaultdict
 from auto_doc import AutoDoc
 from auto_helper import print_errors
 
-debug = False
-path = os.getcwd ()
-if len (sys.argv) == 2: 
-    path = sys.argv[-1]
-elif len (sys.argv) == 3 and sys.argv[2] == "-d":
-    path = sys.argv[1]
-    debug = True 
-    total_time_start = time.time ()
-
 # get error pairs (dict of dicts of list) 
-def generate_error_dict(): 
+def generate_error_dict(path): 
     process = subprocess.run (['pydocstyle', path], stdout=subprocess.PIPE)
     process
     errors = [error.strip () for error in process.stdout.decode ("utf-8").split ("\n")]
@@ -35,19 +26,37 @@ def generate_error_dict():
         error_dict[fname][error_code].append (int (line_number))
     return error_dict
 
+debug = False
+path = os.getcwd ()
+if len (sys.argv) == 2: 
+    path = sys.argv[-1]
+elif len (sys.argv) == 3 and sys.argv[2] == "-d":
+    path = sys.argv[1]
+    debug = True 
+if debug: 
+    overview_dict = defaultdict (int) 
+    error_dict = generate_error_dict (path) 
+    for fname in error_dict: 
+        error_pairs = error_dict[fname] 
+        for i in error_pairs: 
+            overview_dict[i] += len (error_pairs[i])
+    print_errors (overview_dict, "BEFORE")
+    total_time_start = time.time ()
+
+
 # apply auto_doc to every file that has errors 
-error_dict = generate_error_dict() 
+error_dict = generate_error_dict(path) 
 for fname in error_dict: 
     obj = AutoDoc (fname, error_dict[fname])
     obj.execute ()
 if debug:
     total_time_end = time.time () 
-    print ("Total time: ", total_time_end - total_time_start, "seconds")
+    print (f"Total time: {total_time_end - total_time_start} seconds\n")
 
-overview_dict = defaultdict (int) 
-error_dict = generate_error_dict () 
-for fname in error_dict: 
-    error_pairs = error_dict[fname] 
-    for i in error_pairs: 
-        overview_dict[i] += len (error_pairs[i])
-print_errors (overview_dict)
+    overview_dict = defaultdict (int) 
+    error_dict = generate_error_dict (path) 
+    for fname in error_dict: 
+        error_pairs = error_dict[fname] 
+        for i in error_pairs: 
+            overview_dict[i] += len (error_pairs[i])
+    print_errors (overview_dict, "AFTER")
