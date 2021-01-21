ea1c81856d51fa3c95d564ecdf7f68b5b55a5bd8
Ryan Deng <ryan.deng32@gmail.com>
2021-01-20 20:58:34 -0500

    docs: Document auto_helper file and change code style to follow the odyssey coding convention

--- auto_helper.py
+++ auto_helper.py
@@ -1,12 +1,21 @@
-# return whether a line contains any alpha letter 
-def contain_alpha(line): 
+"""Helper functions for auto_doc.py."""
+
+def contain_alpha (line): 
+    """Return True when line contains alphabetic characters and False otherwise."""
     for c in line: 
-        if c.isalpha(): 
+        if c.isalpha (): 
             return True 
     return False 
 
-def get_first_alpha_index(contents, line_index): 
-    while not(contain_alpha(contents[line_index])): 
+def get_first_alpha_index (contents, line_index): 
+    """Return the index for a line that contains alphabetic characters.
+
+    :param contents: a list of lines from a file 
+    :param line_index: the index to start 
+    :return: the first index of contents that contains alphabetic chars starting from line_index 
+    :rtype: int
+    """
+    while not (contain_alpha (contents[line_index])): 
         line_index += 1 
     return line_index
 
@@ -22,36 +31,45 @@ def print_errors (error_pairs, msg=None):
     output.sort ()
     for i in output:
         print (i)
-    print("")
+    print ("")
 
-def get_quote_type(line): 
+def get_quote_type (line): 
+    """Return the quote type used by a docstring given the starting line."""
     quote_types = ['"""', "'''", '"', "'"] 
     curr_type = None 
-    first_line = line.strip() 
+    first_line = line.strip () 
     for quote_type in quote_types: 
-        if first_line.find(quote_type) == 0: 
+        if first_line.find (quote_type) == 0: 
             curr_type = quote_type
             break 
     return curr_type 
 
-def first_non_whitespace_index(line): 
-    return len(line) - len(line.lstrip())
+def first_non_whitespace_index (line): 
+    """Return the index of the first non-whitespace character on the given line.""" 
+    return len (line) - len (line.lstrip ())
 
-def extract_docstring(contents, line_index): 
-    first_line = contents[line_index].strip()
-    curr_type = get_quote_type(contents[line_index]) 
-    if curr_type == "'" or curr_type == '"' or (first_line.count(curr_type) >= 2 and first_line.rfind(curr_type) == len(first_line) - 3): 
+def extract_docstring (contents, line_index): 
+    """Extract information from a docstring.
+    :param contents: a list of lines from a file 
+    :param line_index: the index of the start of the docstring 
+    :return: the start of the docstring, the end of the docstring, and the contents of the docstring
+    :rtype: tuple 
+    """
+    first_line = contents[line_index].strip ()
+    curr_type = get_quote_type (contents[line_index]) 
+    if curr_type == "'" or curr_type == '"' or (first_line.count(curr_type) >= 2 and first_line.rfind (curr_type) == len (first_line) - 3): 
         return (line_index, line_index, contents[line_index])
     start, end = line_index, line_index + 1
     # find the end of docstring 
     while curr_type not in contents[end]: 
         end += 1 
-    raw_docstring = "".join(contents[start: end+1])
+    raw_docstring = "".join (contents[start: end+1])
     return (start, end, raw_docstring)
 
-def adjust_line_num(contents, error_pairs, log): 
-    for error, list_line_nums in error_pairs.items():
-        for i in range(len(list_line_nums)):
+def adjust_line_num (contents, error_pairs, log): 
+    """Adjust line numbers of contents and error_pairs given a log of changes."""
+    for error, list_line_nums in error_pairs.items ():
+        for i in range (len (list_line_nums)):
             num = list_line_nums[i] 
             for log_pair in log: 
                 if num - 1 > log_pair[0]: 
@@ -62,9 +80,13 @@ def adjust_line_num(contents, error_pairs, log):
         error_pairs[error] = list_line_nums
             
         
-def manage_blank_lines(contents, blank_start, log, error_lines_num, keep_one=False): 
+def manage_blank_lines (contents, blank_start, log, error_lines_num, keep_one=False): 
+    """Delete following blank lines starting from index blank_start.
+
+    Keep one blank line when keep_one is set to True
+    """ 
     blank_end = blank_start 
-    while contents[blank_end].strip() == "": 
+    while contents[blank_end].strip () == "": 
         blank_end += 1 
     if keep_one: 
         if blank_end == blank_start: 
